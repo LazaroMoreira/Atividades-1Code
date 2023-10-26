19d5bf845ff62e49658fe362ff469b76bd83e33b
Lázaro Moreira <lazarosant2000@gmail.com>
2023-10-26 13:25:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+//Questao 1 
+let intA = 223
+let intB = 4
+
+let soma = intA + intB
+let subtracao = intA - intB
+let multi = intA * intB
+let dividir = intA / intB
+let modulo = intA % intB
+
+
+console.log(soma);
+
+//Questao 2 
+let anos = 3
+let valorpincipal = 3.000 
+let taxadejuros = 13 / 100
+
+let numerodeanos1 = 1
+let montanteano1 = 3000 * (13/100)
+let juros1 = 390
+
+let numerodeanos2 = 2
+let montanteano2 = 3390 * (13/100)
+let juros2= 441
+
+let numerodeanos3 = 3
+let montanteano3 = 3831 * (13/100)
+let juros3 = 498
+
+let montantes = montanteano1 + montanteano2 + montanteano3
+
+console.log(montantes)
+
+
+//Questao 3 
+let Celsius = 40
+let Fahrenheit = F = (Celsius * 9/5) + 32.
+
+console.log(Fahrenheit)
+
+//Questao 4 
+let media1 = 7
+let media2 = 8
+let media3 = 8
+let media4 = 9
+
+let mediaescolar = media1 + media2 + media3 + media4 
+let mediafinal = mediaescolar / 4
+
+if (mediafinal>=7 ){
+    console.log('Voce foi APROVADO')
+} 
+else if (mediafinal<= 7 ){
+ console.log('Voce foi REPROVADO')   
+}
+
+//Questao 5 
+
+let peso = 73.3
+let altura = 1.80
+let imc = peso / (altura * altura)
+
+console.log(imc)
+
+
+//Questão 6
+
+console.log(dividir)
+console.log(modulo)
+
+
+//Questao 7 
+
+let celularXiaomi = 1800
+let desconto = 20 / 100
+
+let descontoporcentagem = 1800 * 20 / 100
+let descontocelular = celularXiaomi - descontoporcentagem
+
+console.log(descontocelular)
+
+// Questao 8 
+
+let realBR = 4
+let euroUK = 5.2
+let taxadecambio = 0.72 / 100
+let conversordemoeda = realBR * euroUK - taxadecambio
+
+console.log(conversordemoeda)
+
+// Questao 9
+
+let numero = 2;
+
+for (let i = 0; i <= 10; i++) {
+
+    console.log(`${i} x ${numero} = ${i*numero}`)
+ 
+}
+
+
+// Questao 10 
+let fatorizacao = 5 * 4 * 3 * 2 * 1
+console.log(fatorizacao)
+
